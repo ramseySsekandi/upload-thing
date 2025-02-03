c53046c5d750408a21172cf447af70a6dfb70f79
MARLVIN839 <mikemarlvin@gmail.com>
2025-02-03 15:54:28 +0300

    adding hook forms

--- components/ui/CardWithForm.tsx
+++ components/ui/CardWithForm.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+"use client"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -11,26 +11,36 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { IProduct } from "@/types/product"
+import { Plus, } from "lucide-react"
+import { useForm } from "react-hook-form"
 
 export function CardWithForm() {
+  const {register, handleSubmit, reset, formState: {errors}} = useForm<IProduct>()
+  async function onSubmit (data:IProduct) {
+     console.log(data)
+  }
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>File Upload With Upload Thing</CardTitle>
+        <CardDescription></CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="name">Product Title</Label>
+              <Input id="name" placeholder="Title of your Product"
+              {...register('title', {required:true})}    
+              />
+              {errors.title && <span className="bg-red-500">This field is required</span>}
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex">
-        <Button>Deploy</Button>
+        <Button onClick={handleSubmit(onSubmit)} type="submit"><Plus />Save Product</Button>
       </CardFooter>
     </Card>
   )
