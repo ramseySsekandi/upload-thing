a81386c73ee95f1bdcd35c11f8794bae5a887c9b
MARLVIN839 <mikemarlvin@gmail.com>
2025-02-04 17:15:53 +0300

    removing unused var uploadbtn

--- components/ui/CardWithForm.tsx
+++ components/ui/CardWithForm.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { UploadButton, UploadDropzone } from "@/lib/uploadthing"
+import { UploadDropzone } from "@/lib/uploadthing"
 import { IProduct } from "@/types/product"
 import { FileText, Pencil, Plus, } from "lucide-react"
 import Image from "next/image"
